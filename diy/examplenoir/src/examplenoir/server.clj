3c35e8479ae8871dbf5ce5be1861b93468155c91
siscia <simone@mweb.biz>
2012-08-17 11:58:36 +0200

    changed server.clj

--- diy/examplenoir/src/examplenoir/server.clj
+++ diy/examplenoir/src/examplenoir/server.clj
@@ -5,7 +5,9 @@
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
+        port (Integer. (get (System/getenv) "OPENSHIFT_INTERNAL_PORT" "8080"))
+	host (get (System/getenv) "OPENSHIFT_INTERNAL_IP")]
     (server/start port {:mode mode
-                        :ns 'examplenoir})))
+                        :ns 'examplenoir
+                        :jetty-options {:host host}})))
 
